b85f80750fe0575e358cc10d791acf8a718955ed
TJ Holowaychuk <tj@vision-media.ca>
2013-02-13 13:42:48 -0800

    add implementation

---
+++ index.js
@@ -0,0 +1,36 @@
+
+/**
+ * Expose `sort`.
+ */
+
+exports = module.exports = sort;
+
+/**
+ * Sort `el`'s children with the given `fn(a, b)`.
+ *
+ * @param {Element} el
+ * @param {Function} fn
+ * @api public
+ */
+
+function sort(el, fn) {
+  var arr = [].slice.call(el.children).sort(fn);
+  el.innerHTML = '';
+  for (var i = 0; i < arr.length; i++) {
+    el.appendChild(arr[i]);
+  }
+};
+
+/**
+ * Sort descending.
+ *
+ * @param {Element} el
+ * @param {Function} fn
+ * @api public
+ */
+
+exports.desc = function(el, fn){
+  sort(el, function(a, b){
+    return ~fn(a, b) + 1;
+  });
+};
